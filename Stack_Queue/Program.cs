f608c817c73d9d3cda75c5a1b4e23f2846c86877
unknown <satya-1298>
2023-07-10 15:13:01 +0530

    [Satya]Add : Inserting Values in the stack

---
+++ Stack_Queue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Queue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Choose the below options");
+                Console.WriteLine("1-Inserting Elements in Stack");
+                int option =Convert.ToInt32(Console.ReadLine());
+                switch(option) 
+                {
+                    case 1:
+                        Console.WriteLine("Stack Insertion");
+                        StackLinkedList stackLinkedList = new StackLinkedList();
+                        stackLinkedList.Push(70);
+                        stackLinkedList.Push(30);
+                        stackLinkedList.Push(56);
+                        stackLinkedList.Display();
+                        break;
+                    default: Console.WriteLine("Enter Invalid Option");
+                        break;
+                }
+            }
+        } 
+    }
+}
---
+++ Stack_Queue/StackLinkedList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Stack_Queue
+{
+    public class StackLinkedList
+    {
+        public Node top;
+        public StackLinkedList()
+        {
+            top = null;
+        }
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            if (top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = top;
+            }
+            top = node;
+        }
+        public void Display()
+        {
+            Node temp = top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+       
+    }
+}
